e9b216b25b963738525831ab219999a37071c7fa
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-10-19 21:10:54 +0300

    changed footer background

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer(){
     return (
-        <footer className='text-center py-6'>
+        <footer className='text-center py-6 bg-slate-100'>
         <h4 className='font-bold'>Project done by Sarah Abou Dalal</h4>
         <a className='mx-4 underline' href='https://github.com/sarahaboudalal'>GitHub</a>
         <a className='mx-4 underline' href='https://www.linkedin.com/in/sarah-abou-dalal-977891250/'>LinkedIn</a>
